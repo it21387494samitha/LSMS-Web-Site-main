75ecaec8a20486371a100022e91142fef41a1c86
SamithaDhananjaya <dhananjayasamitha68@gmail.com>
2025-05-27 11:52:24 +0530

    gh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Logo.png';
 import { useState } from "react";
 import { HiMenu, HiX } from "react-icons/hi";
 
